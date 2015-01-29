4345f9fb22cf987995780de0203127ad1ad6278f
burrich <jezequel.n@gmail.com>
2015-01-30 00:30:58 +0100

    camera module & tab indent fix

--- conf.lua
+++ conf.lua
@@ -1,13 +1,13 @@
 scale = 1
-debug = true
+debug = false
 
 function love.conf(t)
 	t.title = "Platelover"
 	t.window.width = 256*scale
 	t.window.height = 224*scale
+	t.window.resizable = true
 	
 	if debug then  
 		t.console = true
-		t.window.resizable = true
 	end
 end
--- main.lua
+++ main.lua
@@ -1,141 +1,146 @@
 local sti = require "lib/sti"
 
+require "camera"
+
 function love.load()
-    -- Window size
+	-- Window size
 	windowWidth  = love.graphics.getWidth()
 	windowHeight = love.graphics.getHeight()
 
-    -- Load lua map from Tiled
-    map = sti.new("map/world11")
-    tileSize = map.tilewidth*scale
-    mapPixelWidth = map.width*tileSize
+	-- Set camera bounds
+	camera:setBounds(0, 0, windowWidth, windowHeight)
 
-    -- World
-    physicsWorld = love.physics.newWorld()
-    world = {
-		gravity = 41.32*scale,
-		ground  = windowHeight - tileSize
+	-- Load lua map from Tiled
+	map = sti.new("map/world11")
+	tileSize = map.tilewidth
+	mapPixelWidth = map.width*tileSize
+
+	-- World
+	physicsWorld = love.physics.newWorld()
+	world = {
+		gravity = 41.32,
+		ground  = windowHeight - tileSize*scale
 	}
 
-    -- Collision objects
-    collision = map:initWorldCollision(physicsWorld)
+	-- Collision objects
+	collision = map:initWorldCollision(physicsWorld)
 
-    -- Player
+	-- Player
 	player = {}
-    player.width        = tileSize
-    player.height       = tileSize
-    player.xOrigine     = windowWidth/2 - player.width/2
-    player.x            = player.xOrigine
-    player.y            = world.ground - tileSize
-    player.velocityX    = 0
-    player.velocityY    = 0
-    player.jumpHeight   = 5*tileSize
-    player.ySpeed       = 3
-    player.fallingSpeed = 3
-    player.speed        = 150
-    player.jumping      = false
-    player.falling      = false
-    player.state        = "stand"
-    -- player.moving     = false
+		player.width        = tileSize
+		player.height       = tileSize
+		player.xOrigine     = windowWidth/2-- - player.width/2
+		player.x            = player.xOrigine
+		player.y            = world.ground - tileSize
+		player.velocityX    = 0
+		player.velocityY    = 0
+		player.jumpHeight   = 5*tileSize
+		player.ySpeed       = 3
+		player.fallingSpeed = 3
+		player.speed        = 150
+		player.jumping      = false
+		player.falling      = false
+		player.state        = "stand"
+		-- player.moving     = false
 
-    function player:moveLeft()
-        self.velocityX = -self.speed
-        player.state = "movingLeft"
-    end
+	function player:moveLeft()
+		self.velocityX = -self.speed
+		player.state = "movingLeft"
+	end
 
-    function player:moveRight()
-        self.velocityX = self.speed
-        player.state = "movingRight"
+	function player:moveRight()
+		self.velocityX = self.speed
+		player.state = "movingRight"
 
-    end
+	end
 
-    function player:jump()
-        if not self.jumping then
-            self.velocityY = -self.jumpHeight
-            self.jumping = true
-        end
-    end
+	function player:jump()
+		if not self.jumping then
+			self.velocityY = -self.jumpHeight
+			self.jumping = true
+		end
+	end
 
-    function player:update(dt)
-        if self.jumping or self.falling then
-            self.velocityY = self.velocityY + world.gravity*dt*self.ySpeed
-        end
+	function player:update(dt)
+		if self.jumping or self.falling then
+			self.velocityY = self.velocityY + world.gravity*dt*self.ySpeed
+		end
 
-        nextX = self.x + self.velocityX*dt
-        nextY = self.y + self.velocityY*dt*self.ySpeed
+		nextX = self.x + self.velocityX*dt
+		nextY = self.y + self.velocityY*dt*self.ySpeed
 
-        -- Bottom
-        if self.velocityY > 0 then
-            if self:isColliding(self.x , nextY+self.height) 
-            or self:isColliding(self.x+self.width -1, nextY+self.height) then
+		-- Bottom
+		if self.velocityY > 0 then
+			if self:isColliding(self.x , nextY+self.height) 
+			or self:isColliding(self.x+self.width -1, nextY+self.height) then
 
-                self.jumping = false
-                self.velocityY = 0
-                self.y = nextY - nextY % tileSize
-            else
-                self.y = nextY
-            end
-        end
+				self.jumping = false
+				self.velocityY = 0
+				self.y = nextY - nextY % tileSize
+			else
+				self.y = nextY
+			end
+		end
 
-        -- Top
-        if self.velocityY < 0 then
-            if self:isColliding(self.x , nextY) 
-            or self:isColliding(self.x+self.width -1, nextY) then
+		-- Top
+		if self.velocityY < 0 then
+			if self:isColliding(self.x , nextY) 
+			or self:isColliding(self.x+self.width -1, nextY) then
 
-                self.velocityY = 0
-                self.y = nextY + tileSize - nextY % tileSize
-            else
-                self.y = nextY
-            end
-        end
+				self.velocityY = 0
+				self.y = nextY + tileSize - nextY % tileSize
+			else
+				self.y = nextY
+			end
+		end
 
-        -- Left
-        if self.velocityX < 0 then
-            if self:isColliding(nextX, self.y) 
-            or self:isColliding(nextX, self.y+self.height -1) then
+		-- Left
+		if self.velocityX < 0 then
+			if self:isColliding(nextX, self.y) 
+			or self:isColliding(nextX, self.y+self.height -1) then
 
-                self.x = nextX + tileSize - nextX % tileSize
-            else
-                self.x = nextX
-            end
-        end
+				self.x = nextX + tileSize - nextX % tileSize
+			else
+				self.x = nextX
+			end
+		end
 
-        -- Right
-        if self.velocityX > 0 then
-            if self:isColliding(nextX+self.width, self.y) 
-            or self:isColliding(nextX+self.width, self.y+self.height -1) then
+		-- Right
+		if self.velocityX > 0 then
+			if self:isColliding(nextX+self.width, self.y) 
+			or self:isColliding(nextX+self.width, self.y+self.height -1) then
 
-                self.x = nextX - nextX % tileSize
-            elseif not self:isColliding(nextX, self.y+self.height) then
-                self.falling = true
-                self.x = nextX
-            else
-                self.x = nextX
-            end
-        end
-    end
+				self.x = nextX - nextX % tileSize
+			elseif not self:isColliding(nextX, self.y+self.height) then
+				self.falling = true
+				self.x = nextX
+			else
+				self.x = nextX
+			end
+		end
+	end
 
-    function player:draw()
-        love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-    end
+	function player:draw()
+		love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+	end
 
-    function player:isColliding(x, y)
-	    local tileX = math.floor(x / tileSize) +1
-	    local tileY = math.floor(y / tileSize) +1
+	function player:isColliding(x, y)
+		local tileX = math.floor(x / tileSize) +1
+		local tileY = math.floor(y / tileSize) +1
 
-	    -- print("x : " .. tileX, "y : " .. tileY)
+		-- print("x : " .. tileX, "y : " .. tileY)
 
-        if tileY < 1 or tileY > map.height then
-            return true
-        end
-        -- print("collision : " .. tostring(map.layers["Collision"].data[13][1]))
+		if tileY < 1 or tileY > map.height then
+			return true
+		end
+		-- print("collision : " .. tostring(map.layers["Collision"].data[13][1]))
 		return map.layers["Collision"].data[tileY][tileX]
 	end
 
-    translateX  = 0
+	translateX  = 0
 	translateY  = 0
 
-    -- print("collision : " .. tostring(map.layers["Collision"].data[13][29])) -- x 19
+	-- print("collision : " .. tostring(map.layers["Collision"].data[13][29])) -- x 19
 end
 
 function love.update(dt)
@@ -144,86 +149,86 @@ function love.update(dt)
 	-- 	dt = 0.05
 	-- end
 
-    if love.keyboard.isDown('d', 'right') and player.state ~= "movingLeft" then
-        player:moveRight()
-    end
+	if love.keyboard.isDown('d', 'right') and player.state ~= "movingLeft" then
+		player:moveRight()
+	end
 
-    if love.keyboard.isDown('q', 'left') and player.state ~= "movingRight" then
-        player:moveLeft()
-    end
+	if love.keyboard.isDown('q', 'left') and player.state ~= "movingRight" then
+		player:moveLeft()
+	end
 
-    if love.keyboard.isDown(' ') then
-        player:jump()
-    end
+	if love.keyboard.isDown(' ') then
+		player:jump()
+	end
 
-    map:update(dt)
-    player:update(dt)
+	map:update(dt)
+	player:update(dt)
+
+	camera:setPosition(player.x - windowWidth / 2, 0)
 end
 
 function love.draw()
-	-- TODO: implement camera module
+	camera:set()
+	love.graphics.scale(scale)
 
 	love.graphics.setBackgroundColor(107, 140, 255)
 
-	-- X translation when the player move forward
-	local translation = player.x > player.xOrigine
+	-- Draw Range culls unnecessary tiles
+	-- map:setDrawRange(-translateX, translateY, windowWidth, windowHeight)
 
-	if translation then
-		-- Translation doesn't change at the end of the map
-		-- in order to avoid black screen
-		if (player.x <= (mapPixelWidth - windowWidth/2 - player.width/2)) then
-            translateX  = -(player.x - player.xOrigine) 
-		end
-
-	    love.graphics.translate(translateX, translateY)
-    end
-
-    -- Draw Range culls unnecessary tiles
-    map:setDrawRange(translateX, translateY, windowWidth, windowHeight)
-
-    -- Draw objects
-    map:draw(scale, scale)
+	-- Draw objects
+	map:draw(scale) -- scale
 	player:draw()
 
-    if debug then
-    	-- Draw Collision Map
-    	love.graphics.setColor(255, 0, 0, 255)
-    	map:drawWorldCollision(collision)
+	if debug then
+		-- Draw Collision Map
+		love.graphics.setColor(255, 0, 0, 255)
+		map:drawWorldCollision(collision)
 
-    	-- Reset color and translation
-    	love.graphics.setColor(255, 255, 255, 255)
-    	if translation then
-    		love.graphics.translate(-translateX, 0)
-    	end
+		-- Reset color and translation
+		love.graphics.setColor(255, 255, 255, 255)
+		-- if translation then
+		-- 	camera:setPosition(translateX-(player.x - player.xOrigine), 0)
+		-- end
 
-    	-- Display FPS and player information
-    	love.graphics.print("fps : " .. love.timer.getFPS(), 5, 0)
-    	love.graphics.print("x : " .. string.format("%.1f", player.x), 5, 15)
-    	love.graphics.print("y : " .. string.format("%.1f", player.y), 5, 30)
-    	love.graphics.print("velocity x : " .. string.format("%.1f", player.velocityX), 5, 45)
-        love.graphics.print("velocity y : " .. string.format("%.1f", player.velocityY), 5, 60)
-        love.graphics.print("tile x : " ..  math.floor(player.x / tileSize) +1, 5, 75)
-    	love.graphics.print("tile y : " .. math.floor(player.y / tileSize) +1,5, 90)
+		-- Display FPS and player information
+		love.graphics.print("fps : " .. love.timer.getFPS(), player.x, 0)
+		love.graphics.print("x : " .. string.format("%.1f", player.x), 5, 15)
+		love.graphics.print("y : " .. string.format("%.1f", player.y), 5, 30)
+		love.graphics.print("velocity x : " .. string.format("%.1f", player.velocityX), 5, 45)
+		love.graphics.print("velocity y : " .. string.format("%.1f", player.velocityY), 5, 60)
+		love.graphics.print("tile x : " ..  math.floor(player.x / tileSize) +1, 5, 75)
+		love.graphics.print("tile y : " .. math.floor(player.y / tileSize) +1,5, 90)
 	end
+
+	print("tile x : " ..  math.floor(player.x / tileSize) +1)
+	print("tile y : " .. math.floor(player.y / tileSize) +1)
+	print("player x " .. player.x)
+
+	camera:unset()
 end
 
 function love.resize(w, h)
-    map:resize(w, h)
+	map:resize(w, h)
 end
 
 function love.keypressed(key)
 	if key == 'escape' then
 		love.event.push('quit')
 	end
-    if key == '`' then
-        debug = not debug
-    end
+	if key == '`' then
+		debug = not debug
+	end
 end
 
 function love.keyreleased(key)
-    if key == "q" or key == "left" 
-    or key == "d" or key == "right" then
-        player.velocityX = 0
-        player.state = "stand"
-    end
+	if key == "q" or key == "left" 
+	or key == "d" or key == "right" then
+		player.velocityX = 0
+		player.state = "stand"
+	end
+end
+
+function math.clamp(x, min, max)
+	return x < min and min or (x > max and max or x)
 end
